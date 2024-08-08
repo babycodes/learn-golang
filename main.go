7ff6dbac16deb33d84efdc6db330d7e0d0315b01
babycodes <daemonbackdoor11@gmail.com>
2024-08-08 10:23:15 +0700

    improved my golang learning after 4 years off coding

--- main.go
+++ main.go
@@ -1,56 +1,31 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 )
 
-// Struct dengan json struct tag
-type FactResponse struct {
-	Teks string `json:"text"`
-	Tipe string `json:"type"`
-}
+var badWord = []string{"anjing", "babi", "bangsat", "bajingan"}
 
 func main() {
-	// 1. buat request
-	req, err := http.NewRequest("GET", "https://cat-fact.herokuapp.com/facts/random", nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+
+	registeredUser("wahyu", "bajingan")
+}
+
+func blacklistWord(word string, keyword []string) bool {
+	for _, result := range keyword {
+		if result == word {
+			return true
+		}
+	}
+	return false
+}
+
+func registeredUser(username string, keyword string) {
+	if blacklistWord(keyword, badWord) {
+		fmt.Println("maaf " + username + " kamu sudah di banned karena menggunakan kata kunci " + keyword)
+	} else {
+		fmt.Println("hallo selamat datang " + username)
+
 	}
 
-	// 2. buat vlient
-	client := http.Client{}
-
-	// 3. panggil request dengan client
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
-
-	// tutup response body
-	defer res.Body.Close()
-
-	// 4. baca response body
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
-
-	//  5. convert ke tipe data FactResponse
-	var factResponse FactResponse
-	err = json.Unmarshal(resBody, &factResponse)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
-
-	fmt.Println("result of our API")
-	fmt.Println("teks", factResponse.Teks)
-	fmt.Println("tipe", factResponse.Tipe)
 }
